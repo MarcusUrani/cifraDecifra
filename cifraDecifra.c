cc024a684f79b36f628a97cbacd1cc39cd07bdcf
Marcus <marcus.urani20@gmail.com>
2022-10-05 18:38:17 -0300

    Iniciando os testes

--- cifraDecifra.c
+++ cifraDecifra.c
@@ -12,8 +12,14 @@ int main(void)
         fgets(text, 82, stdin);
         for (j = 0; text[j] != '/n'; j++)
         {
-            int c = text[j] - k;
+            int c = text[j] - n;
+            if (c > 32)
+            {
+                c += 95;
+            }
+            text[j] = c;
         }
+        printf("%s", text);
     }
 
     return 0;
